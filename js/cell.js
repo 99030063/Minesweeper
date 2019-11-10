a658ceed7944f4e61e0ce0ba028ada370b5fcd27
Wesley <99030063@mydavinci.nl>
2019-11-10 21:59:46 +0100

    functional minesweeper. working on animating

--- js/cell.js
+++ js/cell.js
@@ -1,20 +1,87 @@
-function Cell(x, y, w){
-    this.x = x;
-    this.y = y;
+function Cell(i, j, w){
+    this.i = i;
+    this.j = j;
+    this.x = i * w;
+    this.y = j * w;
     this.w = w;
-    this.mine = true;
-    this.revealed = true;
+    this.neighborCount = 0
+
+    this.bee = false;
+    this.revealed = false;
 }
 
-Cell.prototype.show = function(){
-      
-    
-    stroke(0);
-    noFill();
-    rect(this.x, this.y, this.w, this.w);
-    if(this.revealed){
-        if (this.mine){
-            
-        }
+Cell.prototype.show = function(){    
+  stroke(0);
+  noFill();
+  image(image.cell, this.x, this.y, this.w, this.w);
+  if(this.revealed){
+    if (this.mine){
+      noFill();
+      image(image.mine, this.x, this.y, this.w, this.w);
+    }else{
+      stroke(0)
+      noFill();
+      image(image.empty, this.x, this.y, this.w, this.w);
+      if(this.neighborCount > 0){
+        textAlign(CENTER);
+        fill(0);
+        image(number[this.neighborCount-1], this.x, this.y, this.w, this.w);
+      }
     }
+  }
 }
+
+Cell.prototype.countMines = function(){
+  if (this.mine){
+    this.neighborCount = -1;
+    return
+  }
+  var total = 0;
+  for (var xoff = -1; xoff <= +1; xoff++){
+    for (var yoff = -1; yoff <= +1; yoff++){
+      var i = this.i + xoff;
+      var j = this.j + yoff;
+      if(i > -1 && i< cols && j > -1 && j<rows){
+        var neighbor = grid[i][j];
+        if (neighbor.mine){
+          total++
+        }
+      }
+    }
+  }
+  this.neighborCount = total;
+}
+
+Cell.prototype.contains = function (x, y){
+  return (x > this.x && x< this.x + this.w && y > this.y && y < this.y+this.w);
+}
+
+Cell.prototype.reveal = function (){
+  this.revealed = true;
+  if(this.neighborCount == 0){
+    //flood fill
+    this.floodFill();
+  }
+}
+
+Cell.prototype.floodFill = function(){
+  for (var xoff = -1; xoff <= +1; xoff++){
+    for (var yoff = -1; yoff <= +1; yoff++){
+      var i = this.i + xoff;
+      var j = this.j + yoff;
+      if(i > -1 && i< cols && j > -1 && j<rows){
+        var neighbor = grid[i][j];
+        if (!neighbor.mine && !neighbor.revealed){
+          neighbor.reveal();
+        }
+      }
+    }
+  }  
+}
+
+// Cell.prototype.death = function(){
+//   if(this.contains(mouseX, mouseY)){
+//     noFill();
+//     image(image.bomb, this.x, this.y, this.w, this.w);
+//   }
+// }
--- js/sketch.js
+++ js/sketch.js
@@ -1,54 +1,113 @@
-
-
 function make2DArray (cols, rows){
-    var arr = new Array(cols);
-    for (var i = 0; i<arr.length; i++){
-        arr[i] = new Array (rows);
-    }
-    return arr;
+  var arr = new Array(cols);
+  for (var i = 0; i<arr.length; i++){
+    arr[i] = new Array (rows);
+  }
+  return arr;
 }
 
-let img;
+var firstSetup = true;
+var gameover = false;
+var won = false;
 var canvasWidth = 401;
 var canvasHeight = 401;
 var grid;
 var cols;
 var rows;
-var w = 20;
+var w = 40;
 
+var totalMines = 5;
+var number = [];
+
+var images = {
+  mine: undefined,
+  flag: undefined,
+  cell: undefined,
+  bomb: undefined,
+  empty: undefined,
+  wrong: undefined,
+}
+
+function preload(){
+  //assets
+  image.mine = loadImage("../img/minefield_mine.png");
+  image.flag = loadImage("../img/minefield_flag.png");
+  image.cell = loadImage("../img/minefield_space.png");
+  image.empty = loadImage("../img/minefield_empty.png")
+  image.wrong = loadImage("../img/minefield_mine_wrong.png")
+  image.bomb = loadImage("../img/minefield_mine_death.png")
+  for (var i = 0; i < 8; i++){
+    number[i] = loadImage(`../img/minefield_${i+1}.png`);
+  }
+}
 
 function setup(){
-    img = loadImage('img/minefield_1.png');
-    img = loadImage('img/minefield_2.png');
-    img = loadImage('img/minefield_3.png');
-    img = loadImage('img/minefield_4.png');
-    img = loadImage('img/minefield_5.png');
-    img = loadImage('img/minefield_6.png');
-    img = loadImage('img/minefield_7.png');
-    img = loadImage('img/minefield_8.png');
-    img = loadImage('img/minefield_empty.png');
-    img = loadImage('img/minefield_flag.png');
-    img = loadImage('img/minefield_maybe.png');
-    img = loadImage('img/minefield_mine_death.png');
-    img = loadImage('img/minefield_mine_wrong.png');
-    img = loadImage('img/minefield_4.png');
-    img = loadImage('img/minefield_4.png');
-    cols = Math.floor(canvasWidth / w)
-    rows = Math.floor(canvasHeight / w);
-    createCanvas(canvasWidth, canvasHeight);
-    grid = make2DArray(cols, rows);
-    for (var i = 0; i < cols; i++){
-        for (var j = 0; j < cols; j++){
-            grid[i][j] = new Cell(i*w, j*w, w);
-        }
+  createCanvas(canvasWidth, canvasHeight);
+  cols = Math.floor(canvasWidth / w)
+  rows = Math.floor(canvasHeight / w);
+  grid = make2DArray(cols, rows);
+  for (var i = 0; i < cols; i++){
+    for (var j = 0; j < cols; j++){
+      grid[i][j] = new Cell(i, j, w);
     }
+  }
+  //pick total Mines spots
+  var options = [];
+
+  for (var i = 0; i < cols; i++){
+    for(var j = 0; j < rows; j++){
+      options.push([i, j])
+    }
+  }
+  for (var n = 0; n< totalMines; n++){
+    var index = floor(random(options.length));
+    var choice = options[index]
+    var i = choice[0];
+    var j = choice[1];
+    //deletes that spot so it is no longer an option
+    options.splice(index, 1);
+    grid[i][j].mine = true;
+  }
+
+
+  for (var i = 0; i < cols; i++){
+    for (var j = 0; j < cols; j++){
+        grid[i][j].countMines();
+    }
+  }
+}
+
+function lose(){
+  console.log("GAME OVER")
+  for (var i = 0; i < cols; i++){
+    for (var j = 0; j < cols; j++){
+      grid[i][j].revealed = true;
+    }
+  }
+
+}
+
+
+function mousePressed() {
+  for (var i = 0; i < cols; i++){
+    for (var j = 0; j < cols; j++){
+      if(grid[i][j].contains(mouseX, mouseY)){
+        grid[i][j].reveal();
+
+        if(grid[i][j].mine){
+          lose();
+          
+        }
+      }
+    }
+  }
 }
 
 function draw(){
-    background(255);
-    for (var i = 0; i < cols; i++){
-        for (var j = 0; j < cols; j++){
-            grid[i][j].show();
-        }
+  background(255);
+  for (var i = 0; i < cols; i++){
+    for (var j = 0; j < cols; j++){
+      grid[i][j].show();
     }
+  }
 }
